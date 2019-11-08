097f5d68421f8cfed99e4d50d76c3583355a54f7
manny <emmanuel.medina08@gmail.com>
2019-11-08 14:40:55 -0500

    imported browser router  and setup routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route path="/" component={HomePage} exact />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;

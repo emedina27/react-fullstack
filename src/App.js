0bd7dc4a6215f8609fa5a007b2c350a652894826
manny <emmanuel.medina08@gmail.com>
2019-11-08 15:18:19 -0500

    completed adding  pages and routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route path="/" component={HomePage} exact />
+          <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/AboutPage" component={AboutPage} />
+            <Route path="/ArticlePage" component={ArticlePage} />
+            <Route path="/rticlesLis" component={ArticlesList} />
+          </div>
         </div>
       </Router>
     );

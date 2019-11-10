676671286ede18224ed1a5e7e0926b1cd83e78f1
manny <emmanuel.medina08@gmail.com>
2019-11-10 12:21:02 -0500

    created router links for blog template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
+
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <NavBar />
           <div id="page-body">
             <Route path="/" component={HomePage} exact />
-            <Route path="/AboutPage" component={AboutPage} />
-            <Route path="/ArticlePage" component={ArticlePage} />
-            <Route path="/rticlesLis" component={ArticlesList} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/article" component={ArticlePage} />
+            <Route path="/articles-list" component={ArticlesList} />
           </div>
         </div>
       </Router>

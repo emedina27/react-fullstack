c8b980a6aa2d447a15ef635b11312307cb6b59fb
manny <emmanuel.medina08@gmail.com>
2019-11-11 10:40:11 -0500

    created a seperatebackend directory

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
 import NavBar from "./components/NavBar";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/about" component={AboutPage} />
             <Route path="/article/:name" component={ArticlePage} />
             <Route path="/articles-list" component={ArticlesListPage} />
+            <Route component={NotFoundPage} />
           </div>
         </div>
       </Router>
